ddbb5f0bfa4a828c54cfde1c7e0904b6d6f0b1e0
Ernesto Jiménez Villaseñor <netorcido@gmail.com>
2017-08-11 10:03:22 -0500

    El AppComponente se encarga de revisar el estado del usuario.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+
+import { AuthService } from "./services/auth.service";
+
+import * as firebase from "firebase/app";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ){}
+
+  ngOnInit(){
+    this.authService.user
+      .subscribe((user: firebase.User) => {
+        if(!user){
+          this.router.navigate(['/login']);
+        } else{
+          this.router.navigate(['/']);
+        }
+      });
+  }
 }
